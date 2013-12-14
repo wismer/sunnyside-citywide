7238b656c188d9acc95acd628678c2a1b21c321f
wismer <matthewhl@gmail.com>
2013-12-14 15:14:30 -0500

    cleaning up

--- lib/custom_search.rb
+++
@@ -1,7 +0,0 @@
-module Edi
-  include Sunnyside
-  def new_files
-    puts "checking for new files..."
-    return Dir["835/*.txt"].select { |file| file if Filelib.where(filename: file).count == 0 }
-  end
-end 
