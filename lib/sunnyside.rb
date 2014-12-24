aac278f4f9567956efdb01b4842d0b650cd7040d
Matt Long <matthewhl@gmail.com>
2014-12-24 14:20:15 -0500

    remove setup for development;

--- lib/sunnyside.rb
+++ lib/sunnyside.rb
@@ -2,34 +2,35 @@ require 'prawn'
 require 'sequel'
 require 'csv'
 require 'fileutils'
-require "sunnyside/version"
-require 'sunnyside/cash_receipts/cash_receipt'
-require 'sunnyside/ledger/ledger'
-require 'sunnyside/ledger/edi'
-require 'sunnyside/ledger/auth_report'
-require 'sunnyside/ledger/private'
-require 'sunnyside/reports/pdf_report'
-require 'sunnyside/reports/private'
-require 'sunnyside/reports/report'
-require 'sunnyside/query/query'
-require 'sunnyside/ftp'
-require 'sunnyside/menu'
-require 'sunnyside/expiring_auth'
-require 'sunnyside/models/db_setup'
-require 'sunnyside/advanced'
+require_relative 'sunnyside/base.rb'
+# require "sunnyside/version"
+# require 'sunnyside/cash_receipts/cash_receipt'
+# require 'sunnyside/ledger/ledger'
+# require 'sunnyside/ledger/edi'
+# require 'sunnyside/ledger/auth_report'
+# require 'sunnyside/ledger/private'
+# require 'sunnyside/reports/pdf_report'
+# require 'sunnyside/reports/private'
+# require 'sunnyside/reports/report'
+# require 'sunnyside/query/query'
+# require 'sunnyside/ftp'
+# require 'sunnyside/menu'
+# require 'sunnyside/expiring_auth'
+# require 'sunnyside/models/db_setup'
+# require 'sunnyside/advanced'
 
 module Sunnyside
-  DRIVE   = ENV["HOMEDRIVE"]
+  # DRIVE   = ENV["HOMEDRIVE"]
   
-  Sunnyside.create_folders if !Dir.exist?("#{DRIVE}/sunnyside-files")
-  Dir.chdir("R:/Departments/AR Department")
-  DB = Sequel.connect("sqlite://sunnyside.db")
+  # Sunnyside.create_folders if !Dir.exist?("#{DRIVE}/sunnyside-files")
+  # Dir.chdir("R:/Departments/AR Department")
+  # DB = Sequel.connect("sqlite://sunnyside.db")
 
-  if DB.tables.empty?
-    Sunnyside.create_tables
-    Sunnyside.add_providers
-    Sunnyside.add_denial_data
-  end
+  # if DB.tables.empty?
+  #   Sunnyside.create_tables
+  #   Sunnyside.add_providers
+  #   Sunnyside.add_denial_data
+  # end
 
-  require 'sunnyside/models/sequel_classes'
+  # require 'sunnyside/models/sequel_classes'
 end
